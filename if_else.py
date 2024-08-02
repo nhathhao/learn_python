6ae739369c16357bee64ea4bbb48f16069f88edb
Nhat Hao <nhathao05104@gmail.com>
2024-08-02 16:04:31 +0700

    file học if_else

---
+++ if_else.py
@@ -0,0 +1,14 @@
+age = 16
+
+is_conNit = False  # default # initialise the variable
+if age < 10:
+	is_conNit = True
+
+if is_conNit:
+	print("tre nho")
+elif age < 18:
+	print("thieu nien")
+	if age >= 15 and age <= 17:
+		print("thanh thieu nien")
+else:
+	print("nguoi lon")
