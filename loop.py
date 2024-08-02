45daedf1b59b0bfb2454d2aa49be399f2b195a7b
Nhat Hao <nhathao05104@gmail.com>
2024-08-02 20:00:51 +0700

    comment out hết

--- loop.py
+++ loop.py
@@ -17,15 +17,15 @@
 # 		break
 # 	print(i)
 
-s_denominator = 0
-
-for i in range(100):
-	if i == 1:
-		continue
-	if i % 2 == 0:
-		continue
-	s_denominator += 1 / i
-
-s = 1 / s_denominator
-s = round(s, 2)
-print(s)
+# s_denominator = 0
+#
+# for i in range(100):
+# 	if i == 1:
+# 		continue
+# 	if i % 2 == 0:
+# 		continue
+# 	s_denominator += 1 / i
+#
+# s = 1 / s_denominator
+# s = round(s, 2)
+# print(s)
