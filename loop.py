62ee8fc05a9459bf47b8358be69b5ec97dfbc149
Nhat Hao <nhathao05104@gmail.com>
2024-08-02 19:33:41 +0700

    comment out tất cả các ví dụ

--- loop.py
+++ loop.py
@@ -1,18 +1,19 @@
-for i in range (0,2):
-	for j in range (3,5):
-		print(j)
-		print(i)
+# for i in range (0,2):
+# 	for j in range (3,5):
+# 		print(j)
+# 		print(i)
+#
+# print("\n ---- \n")
+#
+# for i in range(10):
+# 	if i == 5:
+# 		continue
+# 	print(i)
+#
+# print("\n ---- \n")
+#
+# for i in range(10):
+# 	if i == 5:
+# 		break
+# 	print(i)
 
-print("\n ---- \n")
-
-for i in range(10):
-	if i == 5:
-		continue
-	print(i)
-
-print("\n ---- \n")
-
-for i in range(10):
-	if i == 5:
-		break
-	print(i)
