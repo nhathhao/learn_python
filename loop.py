d04d6ff4c55c37b96fe94ef12e3b7424db840ed3
Nhat Hao <nhathao05104@gmail.com>
2024-08-02 19:30:55 +0700

    xoá comment vòng lặp ban đầu

--- loop.py
+++ loop.py
@@ -1,7 +1,7 @@
-# for i in range (0,2):
-# 	for j in range (3,5):
-# 		print(j)
-# 		print(i)
+for i in range (0,2):
+	for j in range (3,5):
+		print(j)
+		print(i)
 
 for i in range(10):
 	if i == 5:
