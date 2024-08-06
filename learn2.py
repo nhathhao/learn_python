9542b9faca7a075df04590dbb8d28f6b4602625c
Nhat Hao <nhathao05104@gmail.com>
2024-08-06 22:19:08 +0700

    copy hàm in học sinh từ file function sang learn2

---
+++ learn2.py
@@ -0,0 +1,20 @@
+def main():
+	tenA = "Hao"
+	toanA = 9
+	vanA = 7
+
+	tenB = "Quang"
+	toanB = 8
+	vanB = 9
+
+	print_hocsinh(tenA, toanA, vanA)
+	print_hocsinh(tenB, toanB, vanB)
+
+
+def print_hocsinh(ten, diemToan, diemVan):
+	print("Ten: " + ten)
+	print("Toan: " + str(diemToan))
+	print("Van: " + str(diemVan))
+
+
+main()
