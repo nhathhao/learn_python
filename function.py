777040a7bedb1028896d0189a81bf1a0f0b25873
Nhat Hao <nhathao05104@gmail.com>
2024-08-06 22:23:59 +0700

    uncomment tất cả

--- function.py
+++ function.py
@@ -1,27 +1,33 @@
-# # def print_letter():
-# # 	print("a")
-# # 	print("b")
-# # 	print("c")
-# # print_letter()
-#
-# def print_number(min_munber, max_number, distance):
-# 	for i in range(min_munber, max_number, distance):
-# 		print(i)
-#
-#
-# min_num = input("Số bắt đầu: ")
-# max_num = input("Giới hạn của dãy số: ")
-# dis = input("Khoảng cách các số: ")
-# print_number(int(min_num), int(max_num), int(dis))
-#
-# def add_one(number):
-# 	number += 1
-# 	return number
-#
-#
-# x = 2
-# x = add_one(x)
-# print(x)
+def print_letter():
+	print("a")
+	print("b")
+	print("c")
+
+
+print_letter()
+
+
+def print_number(min_munber, max_number, distance):
+	for i in range(min_munber, max_number, distance):
+		print(i)
+
+
+min_num = input("Số bắt đầu: ")
+max_num = input("Giới hạn của dãy số: ")
+dis = input("Khoảng cách các số: ")
+print_number(int(min_num), int(max_num), int(dis))
+
+
+def add_one(number):
+	number += 1
+	return number
+
+
+x = 2
+x = add_one(x)
+print(x)
+
+
 def main():
 	tenA = "Hao"
 	toanA = 9
