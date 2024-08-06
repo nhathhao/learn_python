09f3e82356acf6b901067bb509f7bb9ea63a5d21
Nhat Hao <nhathao05104@gmail.com>
2024-08-06 20:19:34 +0700

    comment hàm print_letter

--- function.py
+++ function.py
@@ -1,13 +1,12 @@
+# def print_letter():
+# 	print("a")
+# 	print("b")
+# 	print("c")
+# print_letter()
+
 def print_number():
-	for i in range(0, 4):
+	for i in range(0,4):
 		print(i)
 
 
-def print_letter():
-	print("a")
-	print("b")
-	print("c")
-
-
 print_number()
-print_letter()
