f86d4b089a3653c29d718523753c25a80368a544
Nhat Hao <nhathao05104@gmail.com>
2024-08-06 20:57:07 +0700

    comment tất cả

--- function.py
+++ function.py
@@ -1,15 +1,15 @@
-# def print_letter():
-# 	print("a")
-# 	print("b")
-# 	print("c")
-# print_letter()
-
-def print_number(min_munber, max_number, distance):
-	for i in range(min_munber, max_number, distance):
-		print(i)
-
-
-min_num = input("Số bắt đầu: ")
-max_num = input("Giới hạn của dãy số: ")
-dis = input("Khoảng cách các số: ")
-print_number(int(min_num), int(max_num), int(dis))
+# # def print_letter():
+# # 	print("a")
+# # 	print("b")
+# # 	print("c")
+# # print_letter()
+#
+# def print_number(min_munber, max_number, distance):
+# 	for i in range(min_munber, max_number, distance):
+# 		print(i)
+#
+#
+# min_num = input("Số bắt đầu: ")
+# max_num = input("Giới hạn của dãy số: ")
+# dis = input("Khoảng cách các số: ")
+# print_number(int(min_num), int(max_num), int(dis))
