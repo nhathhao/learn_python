cced3fab01997acb6e44b52d6613cbc136161784
Nhat Hao <nhathao05104@gmail.com>
2024-08-06 21:11:04 +0700

    thêm vào return

--- function.py
+++ function.py
@@ -13,3 +13,12 @@
 # max_num = input("Giới hạn của dãy số: ")
 # dis = input("Khoảng cách các số: ")
 # print_number(int(min_num), int(max_num), int(dis))
+
+def add_one(number):
+	number += 1
+	return number
+
+
+x = 2
+num = add_one(x)
+print(num)
